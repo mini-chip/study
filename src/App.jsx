de1207b9b01b6be6faaa46a2ee5ae3a2fc5974ac
mini-chip <75009853+mini-chip@users.noreply.github.com>
2024-05-14 02:17:06 +0900

    feat: todolist 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleTodoChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleClickButton = () => {
+    setTodoList([...todoList, { todo: todo, checked: false }]);
+    setTodo("");
+  };
+
+  const handleToggleCheck = (id) => {
+    const updatedTodoList = [...todoList];
+    updatedTodoList[id].checked = !updatedTodoList[id].checked;
+    setTodoList(updatedTodoList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedTodoList = [...todoList];
+    updatedTodoList.splice(id, 1);
+    setTodoList(updatedTodoList);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="form">
+      <h1>TodoList</h1>
+      <div className="InputSection">
+        <input
+          className="todoInput"
+          type="text"
+          placeholder="할 일을 입력하세요"
+          value={todo}
+          onChange={handleTodoChange}
+        />
+        <button type="button" onClick={handleClickButton}>
+          추가
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="todoSection">
+        <ul>
+          {todoList.map((item, id) => (
+            <li key={id} style={{ display: item.checked ? "none" : "block" }}>
+              <label htmlFor={`checkbox${id}`}>
+                <input
+                  type="checkbox"
+                  id={`checkbox${id}`}
+                  checked={item.checked}
+                  onChange={() => handleToggleCheck(id)}
+                />
+                {item.todo}
+              </label>
+              <button type="button" onClick={() => handleDelete(id)}>
+                삭제
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
